a602ff800b50320ee836065c2724ebeb38dda91a
Fathima Ajvad <146217689+fathimaajvad@users.noreply.github.com>
2024-08-19 10:34:08 +0530

    Add files via upload

---
+++ S3/OOPS/Tutorial/primecomment.java
@@ -0,0 +1,32 @@
+//program to find prime numbers numbers
+//Fathima P Ajvad
+//Roll no:27
+class primecomment
+{
+	public static void main(String args[])
+	{   int num,count=0;
+		 num=Integer.parseInt(args[0]);
+		for(int i=1;i<=num;i++)
+        {
+            for(int j=1;j<=i;j++)
+		   {   
+			    if(i%j==0)
+                {
+                    count++;
+                }
+           }
+                    if(count==2)
+                    {
+                        System.out.println("the prime numbers upto input is"+i);
+                    
+                    }
+                    count=0;
+                
+		
+		  
+		
+		
+		
+}
+}
+}
